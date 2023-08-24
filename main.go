d44a7f7b4586b52f4f53a455e3e43495d5407b62
PWND0U <787772394@qq.com>
2023-08-25 07:45:33 +0800

    ç»è®集成gin

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("项目创建成功,111")
+	// new 一个 Gin Engine 实例
+	e := gin.New()
+	// 注册中间件
+	e.Use(gin.Logger(), gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+	e.NoRoute(func(c *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+	e.Run("0.0.0.0:10001")
 }
